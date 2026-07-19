28ccee20d64335d52d4287902631748da104aed9
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2026-07-19 21:01:18 +0400

    Implemented initial db context with user db set

--- BuildHub/BuildHub/ProjectContext.cs
+++ BuildHub/BuildHub/ProjectContext.cs
@@ -1,6 +1,15 @@
-﻿namespace BuildHub
+﻿using BuildHub.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BuildHub
 {
-    public class ProjectContext
+    public class ProjectContext : DbContext
     {
+        DbSet<User> Users { get; set; }
+
+        public ProjectContext(DbContextOptions<ProjectContext> options) : base(options)
+        {
+            
+        }
     }
 }
