e4415ec2e757a047f55bd993a097d4e0b4a6f622
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2026-07-20 02:12:46 +0400

    Added DbSets(tables) for all models in DbContext

--- BuildHub/BuildHub/ProjectContext.cs
+++ BuildHub/BuildHub/ProjectContext.cs
@@ -5,8 +5,21 @@ namespace BuildHub
 {
     public class ProjectContext : DbContext
     {
-        DbSet<User> Users { get; set; }
-
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contract> Contracts { get; set; }
+        public DbSet<EscrowTransaction> EscrowTransactions { get; set; }
+        public DbSet<Milestone> Milestones { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<QuoteNegotiation> QuoteNegotiations { get; set; }
+        public DbSet<QuoteRequest> QuoteRequests { get; set; }
+        public DbSet<QuoteRequestInvite> QuoteRequestInvites { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<VendorProfile> VendorProfiles { get; set; }
+        
         public ProjectContext(DbContextOptions<ProjectContext> options) : base(options)
         {
             
