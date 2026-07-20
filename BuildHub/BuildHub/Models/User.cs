90f6062716610f9f14a6f9e47dfe12aa9802438e
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-07-20 16:01:53 +0400

    Added user model with its attributes and comments

--- BuildHub/BuildHub/Models/User.cs
+++ BuildHub/BuildHub/Models/User.cs
@@ -2,5 +2,16 @@
 {
     public class User
     {
+        public int Id { get; set; } //system generated 
+        public int UserId { get; set; } //system generated 
+        public string FullName { get; set; } //user input 
+        public string Email { get; set; } //user input 
+        public string PasswordHash { get; set; } //system calculated
+        public string? PhoneNumber { get; set; }  //user input 
+        public string PhoneNumber { get; set; }  //user input 
+        public string Role { get; set; } //user input
+        public string City { get; set; } //user input
+        public bool IsVerified { get; set; } = false; //user input
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; //user input
     }
 }
