0bf656c46a3ee80a3707b4d13d2d8f024bc9c350
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2026-07-21 01:25:46 +0400

    Implemented basic controller for user with GET for getting user by id

--- BuildHub/BuildHub/Controllers/UserController.cs
+++ BuildHub/BuildHub/Controllers/UserController.cs
@@ -1,6 +1,31 @@
-﻿namespace BuildHub.Controllers
+﻿using BuildHub.Models;
+using BuildHub.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildHub.Controllers
 {
-    public class UserController
+    [ApiController]
+    [Route("user")]
+    public class UserController: ControllerBase 
     {
+        private UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        // path: url/user/GetUserById/{id}
+        [HttpGet("GetUserById/{id}")]
+        public IActionResult GetUserById(int id)
+        {
+            //should replace user with response dto
+            User user = _userService.GetUserById(id);
+            
+            if (user == null)
+                return NotFound(new { message = $"User with ID {id} was not found." });
+            
+            return Ok(user);
+        }
     }
 }
