ec4f7618f5d2614720a88db2a323f97ece453807
Mohammed98112 <seaomn27@gmail.com>
2026-07-20 15:55:30 +0400

    QuoteRequest Id + constrain + comment

--- BuildHub/BuildHub/Models/QuoteRequest.cs
+++ BuildHub/BuildHub/Models/QuoteRequest.cs
@@ -2,5 +2,7 @@ namespace BuildHub.Models;
 
 public class QuoteRequest
 {
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int qutoeRequestId { get; set; }  // system generated
     
 }
