814b4ba7c04dff547aa22724496f142fbfe8d7e9
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-20 15:58:41 +0400

    Completed the CategoryRepo

--- BuildHub/BuildHub/Repos/CategoryRepo.cs
+++ BuildHub/BuildHub/Repos/CategoryRepo.cs
@@ -1,6 +1,35 @@
-﻿namespace BuildHub.Repos
+﻿using BuildHub.Models;
+
+namespace BuildHub.Repos
 {
     public class CategoryRepo
     {
+        private ProjectContext context;
+
+        public CategoryRepo(ProjectContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Category> GetAll()
+        {
+            return context.Categories.ToList();
+        }
+
+        public Category GetById(int categoryId)
+        {
+            return context.Categories.FirstOrDefault(c => c.categoryId == categoryId);
+        }
+
+        public void Add (Category category)
+        {
+            context.Categories.Add(category);
+            context.SaveChanges();
+        }
+
+        public void Update()
+        {
+            context.SaveChanges();
+        }
     }
 }
