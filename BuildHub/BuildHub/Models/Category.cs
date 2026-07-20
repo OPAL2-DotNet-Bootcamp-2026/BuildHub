02b011aacfaf4f73b0ea3a9ef723184a25039229
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-20 15:16:27 +0400

    Added the category variables

--- BuildHub/BuildHub/Models/Category.cs
+++ BuildHub/BuildHub/Models/Category.cs
@@ -3,4 +3,9 @@ namespace BuildHub.Models;
 public class Category
 {
     
+    public int categoryId { get; set; }
+    public int parentCategoryId { get; set; }
+    public string nameAr {  get; set; }
+    public string nameEn { get; set; }
+    public string type {  get; set; }
 }
