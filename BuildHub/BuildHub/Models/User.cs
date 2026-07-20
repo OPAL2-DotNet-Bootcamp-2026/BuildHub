3ecdd99a4e2f80be695976f95fc8731437a318e4
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-07-20 16:03:19 +0400

    Added constraints annotation for user model

--- BuildHub/BuildHub/Models/User.cs
+++ BuildHub/BuildHub/Models/User.cs
@@ -1,17 +1,35 @@
-﻿namespace BuildHub.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.InteropServices;
+
+namespace BuildHub.Models
 {
     public class User
     {
-        public int Id { get; set; } //system generated 
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; } //system generated 
+        [Required]
+        [MaxLength(150)]
         public string FullName { get; set; } //user input 
+        [Required]
+        [MaxLength(150)]
         public string Email { get; set; } //user input 
+        [Required]
+        [MaxLength(255)]
         public string PasswordHash { get; set; } //system calculated
-        public string? PhoneNumber { get; set; }  //user input 
+        [Required]
+        [MaxLength(20)]
         public string PhoneNumber { get; set; }  //user input 
+        [Required]
+        [MaxLength(20)]
         public string Role { get; set; } //user input
+        [Required]
+        [MaxLength(100)]
         public string City { get; set; } //user input
+        [Required]
         public bool IsVerified { get; set; } = false; //user input
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; //user input
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; //default value
     }
 }
