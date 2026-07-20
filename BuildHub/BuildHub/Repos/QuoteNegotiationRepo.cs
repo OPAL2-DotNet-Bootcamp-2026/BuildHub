b8cf15b114ba4e23669ee8cc1278dea79c9f51e6
Louai Al ameiri <Louai199947@gmail.com>
2026-07-20 16:05:04 +0400

    QuoteNegotiationRepo added

--- BuildHub/BuildHub/Repos/QuoteNegotiationRepo.cs
+++ BuildHub/BuildHub/Repos/QuoteNegotiationRepo.cs
@@ -2,5 +2,14 @@
 {
     public class QuoteNegotiationRepo
     {
+        private ProjectContext context;
+
+        public QuoteNegotiationRepo(ProjectContext _context)
+        {
+            context = _context;
+
+        }
+
+
     }
 }
