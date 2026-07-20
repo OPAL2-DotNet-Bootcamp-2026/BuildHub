cbc95202c0c047f436976dc0751f3185d0bb80b0
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-20 15:30:23 +0400

    Added the data annotations for the category

--- BuildHub/BuildHub/Models/Category.cs
+++ BuildHub/BuildHub/Models/Category.cs
@@ -1,11 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BuildHub.Models;
 
 public class Category
 {
-    
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int categoryId { get; set; }
-    public int parentCategoryId { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int? parentCategoryId { get; set; }
+
+    [Required]
+    [MaxLength(150)]
     public string nameAr {  get; set; }
+
+    [Required]
+    [MaxLength(150)]
     public string nameEn { get; set; }
+
+    [Required]
+    [MaxLength(20)]
     public string type {  get; set; }
 }
