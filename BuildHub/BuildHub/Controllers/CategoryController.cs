6304a9a68d11fc0aab69600a4d766ac839e99da5
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-21 09:29:06 +0400

    Applied dependency injection and dependency invertion

--- BuildHub/BuildHub/Controllers/CategoryController.cs
+++ BuildHub/BuildHub/Controllers/CategoryController.cs
@@ -1,6 +1,14 @@
-﻿namespace BuildHub.Controllers
+﻿using BuildHub.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildHub.Controllers
 {
-    public class CategoryController
+    public class CategoryController : ControllerBase
     {
+        private CategoryService categoryService;
+        public CategoryController (CategoryService _categoryService)
+        {
+            categoryService = _categoryService;
+        }
     }
 }
