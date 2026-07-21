aeb0eaf49dc30a8920be5357137cfd959dc2490b
Louai Al ameiri <Louai199947@gmail.com>
2026-07-21 10:33:10 +0400

    QuoteNegotiationRepo service setup

--- BuildHub/BuildHub/Services/QuoteNegotiationService.cs
+++ BuildHub/BuildHub/Services/QuoteNegotiationService.cs
@@ -1,6 +1,19 @@
-﻿namespace BuildHub.Services
+﻿using BuildHub.Repos;
+
+namespace BuildHub.Services
 {
     public class QuoteNegotiationService
     {
+        
+        private QuoteNegotiationRepo _repo;
+
+        public QuoteNegotiationService(QuoteNegotiationRepo repo)
+        {
+            repo = _repo;
+        }
+
+
+
+
     }
 }
