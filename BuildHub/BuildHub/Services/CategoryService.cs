02b4d05b40e5f6ff77c4e81da5c7fea147c4e3ec
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-21 12:05:32 +0400

    Updated the Category service

--- BuildHub/BuildHub/Services/CategoryService.cs
+++ BuildHub/BuildHub/Services/CategoryService.cs
@@ -1,6 +1,25 @@
-﻿namespace BuildHub.Services
+﻿using BuildHub.Models;
+using BuildHub.Repos;
+
+namespace BuildHub.Services
 {
     public class CategoryService
     {
+        private CategoryRepo repo;
+        public CategoryService (CategoryRepo _repo)
+        {
+            repo = _repo;
+        }
+
+        public List<Category> GetAll()
+        {
+            return repo.GetAll();
+        }
+
+        public Category GetById(int categoryId)
+        {
+            return repo.GetById(categoryId);
+        }
+
     }
 }
