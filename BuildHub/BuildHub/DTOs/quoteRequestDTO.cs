e5c16a1fd2a8148feb5d0ea3ca2e7fb98714bb35
Mohammed98112 <seaomn27@gmail.com>
2026-07-21 18:11:48 +0400

    Status

--- BuildHub/BuildHub/DTOs/quoteRequestDTO.cs
+++ BuildHub/BuildHub/DTOs/quoteRequestDTO.cs
@@ -25,4 +25,5 @@ namespace BuildHub.DTOs
         public string Description { get; set; }
         public DateTime? Deadline { get; set; }
         public string VisibilityType { get; set; }
+        public string Status { get; set; }
 }
