dc04b980656292a33a7eed82f9974d1b26f0467a
Mohammed98112 <seaomn27@gmail.com>
2026-07-21 18:10:09 +0400

    finished from QuoteRequestInputDTOs

--- BuildHub/BuildHub/DTOs/quoteRequestDTO.cs
+++ BuildHub/BuildHub/DTOs/quoteRequestDTO.cs
@@ -3,15 +3,21 @@
 namespace BuildHub.DTOs
 {
     public class QuoteRequestInputDTOs
+    {
+
         [Required(ErrorMessage = "Value should not be null.")]
         public int ProjectId { get; set; }
 
         [Required(ErrorMessage = "Value should not be null.")]
         public int CategoryId { get; set; }
+
         [Required(ErrorMessage = "Value should not be null.")]
         public string Description { get; set; }
+
         public DateTime? Deadline { get; set; }
+
         [Required(ErrorMessage = "Value should not be null.")]
         [MaxLength(10)]
         public string VisibilityType { get; set; }
+    }
 }
