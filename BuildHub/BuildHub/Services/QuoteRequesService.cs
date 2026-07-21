39ada23a2066e26ae2669b7b3be70a1166e7a65c
Mohammed98112 <seaomn27@gmail.com>
2026-07-21 17:22:02 +0400

    private QuoteRequestRepo repo;

--- BuildHub/BuildHub/Services/QuoteRequesService.cs
+++ BuildHub/BuildHub/Services/QuoteRequesService.cs
@@ -6,6 +6,9 @@ namespace BuildHub.Services
 {
     public class QuoteRequestService
     {
+        //QuoteRequestRepo repo = new QuoteRequestRepo();
+        //apply dependency inversion 
+        private QuoteRequestRepo repo;
         
         
     }
