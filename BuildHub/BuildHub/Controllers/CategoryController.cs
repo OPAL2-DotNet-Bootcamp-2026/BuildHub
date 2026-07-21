a215973463ded287cebf6f8b9f18e7c1cadeefce
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-21 16:14:10 +0400

    Added for the CategoryControllers the GetAll and GetById

--- BuildHub/BuildHub/Controllers/CategoryController.cs
+++ BuildHub/BuildHub/Controllers/CategoryController.cs
@@ -12,5 +12,17 @@ namespace BuildHub.Controllers
         {
             categoryService = _categoryService;
         }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            return Ok (categoryService.GetAll());
+        }
+
+        [HttpGet("GetById")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            return Ok(categoryService.GetById(id));
+        }
     }
 }
