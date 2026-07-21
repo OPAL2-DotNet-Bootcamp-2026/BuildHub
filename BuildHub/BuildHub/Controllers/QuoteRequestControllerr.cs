9d65c77024e2b30eecfc4a12bd37de1e7d10519a
Mohammed98112 <seaomn27@gmail.com>
2026-07-21 17:32:50 +0400

    quoteRequest controller class

---
+++ BuildHub/BuildHub/Controllers/QuoteRequestControllerr.cs
@@ -0,0 +1,13 @@
+﻿using BuildHub.DTOs;
+using BuildHub.Models;
+using BuildHub.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildHub.Controllers
+{
+  
+    
+}
+
