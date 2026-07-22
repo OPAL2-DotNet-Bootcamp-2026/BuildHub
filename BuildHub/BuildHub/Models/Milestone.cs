e48e5214836e978106c89a03a9c1629faa5f06c1
Louai Al ameiri <Louai199947@gmail.com>
2026-07-22 09:18:45 +0400

    opject added

--- BuildHub/BuildHub/Models/Milestone.cs
+++ BuildHub/BuildHub/Models/Milestone.cs
@@ -1,6 +1,40 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BuildHub.Models;
 
 public class Milestone
 {
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int milestoneId { get; set; } //System genreated 
+
+
+    [Required]
+    [ForeignKey("Contract")]
+    public int contractId { get; set; } //User input
+
+
+    [Required]
+    [MaxLength(150)]
+    public string title { get; set; } //User input 
+
+
+    [Required]
+    [Range(0.01, double.MaxValue,ErrorMessage = "Should be more than 0 ")]
+    public decimal amount { get; set; } // User input
+
+
+    [Required]
+    [Range(0,int.MaxValue,ErrorMessage ="Should be more than 0")]
+    public int orderIndex { get; set; } // user input 
+
+
+    [Required]
+    public string status { get; set; }//Defualt value 
+
+
+
+    public DateTime? dueDate { get; set; } // user input 
+
     
 }
