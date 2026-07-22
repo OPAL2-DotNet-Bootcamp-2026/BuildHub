053da0f478d613c8cb5363402734ab5440f6df2a
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-07-22 11:13:27 +0400

    Added source tags to VendorProfile model

--- BuildHub/BuildHub/Models/VendorProfile.cs
+++ BuildHub/BuildHub/Models/VendorProfile.cs
@@ -8,24 +8,24 @@ public class VendorProfile
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int VendorProfileID { get; set; }
+    public int VendorProfileID { get; set; } //system generated
     [Required]
     [ForeignKey("user")]
-    public int UserId { get; set; }
-    public User User { get; set; }
+    public int UserId { get; set; } //user input
+    public User User { get; set; } //navigation property 
     [Required]
     [MaxLength(150)]
-    public string CompanyName { get; set; }
+    public string CompanyName { get; set; } //user input
     [Required]
     [MaxLength(20)]
-    public VendorType VendorType { get; set; }
+    public VendorType VendorType { get; set; } //from list
     [Required]
     [MaxLength(100)]
-    public string City { get; set; }
+    public string City { get; set; } //user input
     [Required]
-    public bool IsVerfied { get; set; } = false;
-    public double? AverageRating { get; set; } = 0;
+    public bool IsVerfied { get; set; } = false; //defualt values
+    public double? AverageRating { get; set; } = 0; //defualt values
     [Required]
     [Range(0, double.MaxValue)]
-    public double Balance { get; set; } = 0;
+    public double Balance { get; set; } = 0; //defualt values
 }
