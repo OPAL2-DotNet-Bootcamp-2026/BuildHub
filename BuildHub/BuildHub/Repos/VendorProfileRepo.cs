59d9906a2ac8a5d99be9ab9e8485e398eb772659
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-07-22 12:09:08 +0400

    Implemented all the basic crud related repos for VendorProfileRepo

--- BuildHub/BuildHub/Repos/VendorProfileRepo.cs
+++ BuildHub/BuildHub/Repos/VendorProfileRepo.cs
@@ -1,7 +1,42 @@
 ﻿
+using BuildHub.Models;
+
 namespace BuildHub.Repos
 {
     public class VendorProfileRepo
     {
+        private ProjectContext _projectContext;
+
+        public VendorProfileRepo(ProjectContext projectContext)
+        {
+            _projectContext = projectContext;
+        }
+
+        public List<VendorProfile> GetAll()
+        {
+            return _projectContext.VendorProfiles.ToList();
+        }
+
+        public VendorProfile? GetById(int id)
+        {
+            return _projectContext.VendorProfiles.FirstOrDefault(v => v.VendorProfileID == id);
+        }
+
+        public int Add(VendorProfile vendorProfile)
+        {
+            _projectContext.VendorProfiles.Add(vendorProfile);
+            _projectContext.SaveChanges();
+            return vendorProfile.VendorProfileID;
+        }
+
+        public void Update()
+        {
+            _projectContext.SaveChanges();
+        }
+
+        public void Delete(VendorProfile vendorProfile)
+        {
+            _projectContext.VendorProfiles.Remove(vendorProfile);
+        }
     }
 }
