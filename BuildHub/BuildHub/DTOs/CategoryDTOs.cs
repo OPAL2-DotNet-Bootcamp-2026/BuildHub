d0a1b6458ad7ec92164892f2deb40ce0efae007b
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-22 10:58:45 +0400

    Implement the CategoryInputDTOs

--- BuildHub/BuildHub/DTOs/CategoryDTOs.cs
+++ BuildHub/BuildHub/DTOs/CategoryDTOs.cs
@@ -4,5 +4,13 @@
     // Input DTOs 
     public class CategoryInputDTOs
     {
+        [Required(ErrorMessage = "This field is required")]
+        public string nameAr { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public string nameEn { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public string type { get; set; }
     }
 }
