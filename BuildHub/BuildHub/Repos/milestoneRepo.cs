008bf19f400f7845dc5065e09af13377e29b693f
Louai Al ameiri <Louai199947@gmail.com>
2026-07-22 09:39:52 +0400

    milestone repo added done

---
+++ BuildHub/BuildHub/Repos/milestoneRepo.cs
@@ -0,0 +1,50 @@
+﻿using BuildHub.Models;
+
+namespace BuildHub.Repos
+{
+    public class milestoneRepo
+    {
+        private ProjectContext context;
+
+
+        public milestoneRepo(ProjectContext _context)
+        {
+            context = _context;
+
+        }
+        
+        public List<Milestone> GetAllMilestones()
+        {
+            return context.Milestones.ToList();
+        }
+
+        public Milestone GetAllMilestoneById(int id)
+
+        { 
+            return context.Milestones.FirstOrDefault(m => m.milestoneId == id);
+
+        }
+        public void AddMilestone(Milestone milestone)
+        { 
+            context.Milestones.Add(milestone);
+            context.SaveChanges();
+        }
+
+        public void Ubdate()
+        {
+            context.SaveChanges();
+        }
+
+        public void Delete (Milestone milestone)
+        {
+            context.Milestones.Remove(milestone);
+            context.SaveChanges() ;
+        }
+
+
+
+
+
+
+    }
+}
