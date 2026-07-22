f3cc991459f611ff5f321d162fff227284be0518
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-07-22 10:58:20 +0400

    Implemented the attributes for the VendorProfile model

--- BuildHub/BuildHub/Models/VendorProfile.cs
+++ BuildHub/BuildHub/Models/VendorProfile.cs
@@ -3,4 +3,12 @@ namespace BuildHub.Models;
 public class VendorProfile
 {
     
+    public int VendorProfileID { get; set; }
+    public int UserId { get; set; }
+    public string CompanyName { get; set; }
+    public VendorType VendorType { get; set; }
+    public string City { get; set; }
+    public bool IsVerfied { get; set; }
+    public double AverageRating { get; set; }
+    public double Balance { get; set; }
 }
