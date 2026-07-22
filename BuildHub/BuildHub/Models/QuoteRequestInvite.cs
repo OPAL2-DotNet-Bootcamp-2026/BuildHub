059b027a1b5b898facb320959ca68305bc870644
Mohammed98112 <seaomn27@gmail.com>
2026-07-22 10:57:30 +0400

    DatabaseGenerated

--- BuildHub/BuildHub/Models/QuoteRequestInvite.cs
+++ BuildHub/BuildHub/Models/QuoteRequestInvite.cs
@@ -2,6 +2,7 @@ namespace BuildHub.Models;
 
 public class QuoteRequestInvite
 {
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int inviteId { get; set; }  // system generated
     
 }
