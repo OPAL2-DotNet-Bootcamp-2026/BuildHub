ef4b45aaf6a545ff2473a475929ac182f407f2a9
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-22 11:15:53 +0400

    Added some comments for the Category service

--- BuildHub/BuildHub/Services/CategoryService.cs
+++ BuildHub/BuildHub/Services/CategoryService.cs
@@ -11,22 +11,26 @@ namespace BuildHub.Services
             repo = _repo;
         }
 
+        // Function to get all the data 
         public List<Category> GetAll()
         {
             return repo.GetAll();
         }
 
+        // Function to get by ID only 
         public Category GetById(int categoryId)
         {
             return repo.GetById(categoryId);
         }
 
+        // Function used to add or create 
         public int Add (Category category)
         {
             repo.Add(category);
             return category.categoryId;
         }
         
+        // Function to update 
         public bool UpdateType (int categoryId , string newType)
         {
             Category category = repo.GetById(categoryId);
@@ -39,6 +43,7 @@ namespace BuildHub.Services
             return true;
         }
 
+        // Function to delete
         public bool Delete (int categoryId)
         {
             Category category = repo.GetById(categoryId);
