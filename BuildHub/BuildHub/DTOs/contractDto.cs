6a17629ed093e0bb99db604482c778a346f09015
Louai Al ameiri <Louai199947@gmail.com>
2026-07-23 11:43:19 +0400

    ContractOutputDTO created

--- BuildHub/BuildHub/DTOs/contractDto.cs
+++ BuildHub/BuildHub/DTOs/contractDto.cs
@@ -8,18 +8,37 @@ namespace BuildHub.DTOs
         public class ContractInputDto
         {
 
-            [Required(ErrorMessage = "Contract Id is required .")]
+            [Required]
             public int quoteId {  get; set; }
 
 
 
-            [Required(ErrorMessage = "Contract Id is required .")]
+            [Required]
             public decimal totalAmount { get; set; }
 
 
 
-            [Required(ErrorMessage = "Contract Id is required .")]
+            [Required(ErrorMessage = "Payment Type is required .")]
             public string paymentType { get; set; }
+
+        }
+
+
+        // Output
+
+        public class ContractOutputDTO
+        {
+            public int contractId { get; set; }
+            public int quoteId { get; set; }
+
+            public decimal totalAmount { get; set; }
+
+            public string paymentType { get; set; }
+
+            public string status { get; set; }
+
+            public DateTime signedAt { get; set; }
+
 
         }
 
