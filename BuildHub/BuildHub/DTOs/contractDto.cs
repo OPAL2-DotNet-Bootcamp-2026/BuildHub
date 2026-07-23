13cf8a8cf0da1a71739b1e27cec95a15bfd5b73f
Louai Al ameiri <Louai199947@gmail.com>
2026-07-23 11:37:13 +0400

    ContractInputDto created

--- BuildHub/BuildHub/DTOs/contractDto.cs
+++ BuildHub/BuildHub/DTOs/contractDto.cs
@@ -1,7 +1,27 @@
-﻿namespace BuildHub.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuildHub.DTOs
 {
     public class contractDto
     {
+        //Input
+        public class ContractInputDto
+        {
+
+            [Required(ErrorMessage = "Contract Id is required .")]
+            public int quoteId {  get; set; }
+
+
+
+            [Required(ErrorMessage = "Contract Id is required .")]
+            public decimal totalAmount { get; set; }
+
+
+
+            [Required(ErrorMessage = "Contract Id is required .")]
+            public string paymentType { get; set; }
+
+        }
 
 
 
