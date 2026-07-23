0c0203cc5ff0b2c9aaf992a37cb612741bfe2703
Mohammed98112 <seaomn27@gmail.com>
2026-07-23 09:26:52 +0400

    finished from controller

--- BuildHub/BuildHub/Controllers/QuoteRequestInviteController .cs
+++ BuildHub/BuildHub/Controllers/QuoteRequestInviteController .cs
@@ -8,6 +8,7 @@ namespace BuildHub.Controllers
     [ApiController]
     [Route("QuoteRequestInvite")]
     public class QuoteRequestInviteController : ControllerBase
+    {
         //QuoteRequestInviteService quoteRequestInviteService = new QuoteRequestInviteService();
         //apply dependency inversion concept
         private QuoteRequestInviteService quoteRequestInviteService;
@@ -15,7 +16,7 @@ namespace BuildHub.Controllers
         {
             quoteRequestInviteService = _quoteRequestInviteService;
         }
-    
+
         [HttpGet("GetAllQuoteRequestInvite")]
         public IActionResult GetAllQuoteRequestInvite()
         {
@@ -26,32 +27,42 @@ namespace BuildHub.Controllers
                 return Ok(result);
             }
             return NoContent(); //204 no data
+        }
+
         [HttpGet("GetQuoteRequestInviteById/{id}")]
         public IActionResult GetQuoteRequestInviteById([FromRoute] int id)
+        {
             QuoteRequestInviteOutputDTOs invite = quoteRequestInviteService.GetQuoteRequestInviteById(id);
             if (invite == null)
             {
                 return NotFound(); // 404 notfound
             }
             return Ok(invite); //200 succeeded
+        }
+
         [HttpPost("AddDTO")]
         public IActionResult AddDTO([FromBody] QuoteRequestInviteInputDTOs invite)
         {
             int inviteId = quoteRequestInviteService.Create(invite);
             return Ok(new { InviteId = inviteId }); //200, InviteId=1
         }
-    
+
         [HttpPut("UpdateStatus/{inviteId}")]
         public IActionResult UpdateStatus([FromRoute] int inviteId, [FromQuery] string newStatus)
+        {
             bool updated = quoteRequestInviteService.UpdateStatus(inviteId, newStatus);
             if (!updated)
                 return NotFound();
             return Ok("Updated successfully");
-}
+        }
 
         [HttpDelete("Delete/{id}")]
         public IActionResult Delete([FromRoute] int id)
+        {
             bool deleted = quoteRequestInviteService.Delete(id);
             if (!deleted)
                 return NotFound();
             return Ok("deleted successfully");
+        }
+    }
+}
