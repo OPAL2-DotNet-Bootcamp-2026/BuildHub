dfb8a68da447cd3a77ff90fde549f38e3dec0d68
Mohammed98112 <seaomn27@gmail.com>
2026-07-23 12:34:04 +0400

    created quoteRequestId, foreign key, constraint: not null, references QuoteRequest

--- BuildHub/BuildHub/Models/Quote.cs
+++ BuildHub/BuildHub/Models/Quote.cs
@@ -5,4 +5,7 @@ public class Quote
     [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int quoteId { get; set; }  // system generated
     
+    public int quoteRequestId { get; set; }   // foreign key
+    [ForeignKey("QuoteRequest")]
+    public QuoteRequest QuoteRequest { get; set; }
 }
