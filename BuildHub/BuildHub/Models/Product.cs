413e237a4dd93efe8f505c3d62e39a9d28b2b828
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-23 08:17:21 +0400

    Added the Product entity attributes

--- BuildHub/BuildHub/Models/Product.cs
+++ BuildHub/BuildHub/Models/Product.cs
@@ -2,6 +2,12 @@ namespace BuildHub.Models;
 
 public class Product
 {
-    
+    public int productId { get; set; }
+    public int vendorProfileId { get; set; }
+    public int categoryId { get; set; }
+    public string name { get; set; }
+    public string unit { get; set; }
+    public int currentPrice { get; set; }
+    public bool isAvailable { get; set; }
 
 }
