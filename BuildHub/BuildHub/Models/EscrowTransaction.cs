51e3536d9ee369fa825b936e7117ae5766375032
Mohammed98112 <seaomn27@gmail.com>
2026-07-23 15:50:37 +0400

    created escrowTransactionId, primary key, constraint: automatically generated

--- BuildHub/BuildHub/Models/EscrowTransaction.cs
+++ BuildHub/BuildHub/Models/EscrowTransaction.cs
@@ -1,6 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BuildHub.Models;
 
 public class EscrowTransaction
 {
-    
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int escrowTransactionId { get; set; }
 }
