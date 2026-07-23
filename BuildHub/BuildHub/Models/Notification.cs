ea1c55c5a57ac6ca52a3b5073589e6add52891e4
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-23 07:50:34 +0400

    Added the Notification attributes

--- BuildHub/BuildHub/Models/Notification.cs
+++ BuildHub/BuildHub/Models/Notification.cs
@@ -3,5 +3,9 @@ namespace BuildHub.Models;
 public class Notification
 {
     public int notificationId {  get; set; }
-
+    public int userId { get; set; }
+    public string title { get; set; }
+    public string type { get; set; }
+    public bool isRead { get; set; }
+    public DateTime createdAt { get; set; }
 }
