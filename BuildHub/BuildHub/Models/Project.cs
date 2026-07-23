4ac7e3ccb17d63368812b0d42b84a3a27fc7b33e
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-07-23 17:16:08 +0400

    Added data annotations for Project model

--- BuildHub/BuildHub/Models/Project.cs
+++ BuildHub/BuildHub/Models/Project.cs
@@ -1,16 +1,32 @@
 using BuildHub.enums;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BuildHub.Models;
 
 public class Project
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int ProjectId { get; set; } //system genterated 
+    [Required]
+    [ForeignKey("User")]
     public int ClientId { get; set; } //user input 
+    public User User { get; set; }
+    [Required]
+    [MaxLength(200)]
     public string Title { get; set; } //user input 
-    public string Description { get; set; } //user input 
+    public string? Description { get; set; } //user input 
+    [Required]
+    [MaxLength(100)]
     public string City { get; set; } //user input 
+    [Required]
+    [Range(0, double.MaxValue)]
+    [Column(TypeName = "decimal(12,2)")]
     public decimal Budget { get; set; } //user input 
-    public ProjectStatus Status { get; set; } //from list 
+    [Required]
+    [MaxLength(20)]
+    public ProjectStatus Status { get; set; } = ProjectStatus.Draft; //from list 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow; //default value 
 }
