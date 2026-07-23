799386d58e5bdcd4d8c278b4341fd75eb851bffc
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-07-23 16:11:35 +0400

    Added al the attributes for project model

--- BuildHub/BuildHub/Models/Project.cs
+++ BuildHub/BuildHub/Models/Project.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace BuildHub.Models;
 
 public class Project
 {
-    
+    public int ProjectId { get; set; }
+    public int ClientId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string City { get; set; }
+    public decimal Budget { get; set; }
+    public string Status { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
