dff39d75713ef844d85c16a216a82fdc4e7868fc
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-07-23 16:14:17 +0400

    Added source tags for project model

--- BuildHub/BuildHub/Models/Project.cs
+++ BuildHub/BuildHub/Models/Project.cs
@@ -4,12 +4,12 @@ namespace BuildHub.Models;
 
 public class Project
 {
-    public int ProjectId { get; set; }
-    public int ClientId { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public string City { get; set; }
-    public decimal Budget { get; set; }
-    public string Status { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public int ProjectId { get; set; } //system genterated 
+    public int ClientId { get; set; } //user input 
+    public string Title { get; set; } //user input 
+    public string Description { get; set; } //user input 
+    public string City { get; set; } //user input 
+    public decimal Budget { get; set; } //user input 
+    public string Status { get; set; } //from list 
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow; //default value 
 }
