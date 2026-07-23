3d8abf48917b3dcfa12f077fc328dfb95b0d85c4
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-23 07:54:34 +0400

    Added data annotation to make all the attributes not null.

--- BuildHub/BuildHub/Models/Notification.cs
+++ BuildHub/BuildHub/Models/Notification.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BuildHub.Models;
 
 public class Notification
 {
+    [Key]
     public int notificationId {  get; set; }
+    [Required]
     public int userId { get; set; }
+    [Required]
     public string title { get; set; }
+    [Required]
     public string type { get; set; }
+    [Required]
     public bool isRead { get; set; }
+    [Required]
     public DateTime createdAt { get; set; }
 }
