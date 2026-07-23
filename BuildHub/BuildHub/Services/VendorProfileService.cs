9cbd0ee92a44b4c3cfe7f30aa25069ea9db89a7a
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-07-23 12:21:06 +0400

    Implemented get all vendor profiles service in VendorProfileService

--- BuildHub/BuildHub/Services/VendorProfileService.cs
+++ BuildHub/BuildHub/Services/VendorProfileService.cs
@@ -1,7 +1,20 @@
-﻿namespace BuildHub.Services
+﻿using BuildHub.Models;
+using BuildHub.Repos;
+
+namespace BuildHub.Services
 {
     public class VendorProfileService
     {
+        private VendorProfileRepo _vendorProfileRepo;
 
+        public VendorProfileService(VendorProfileRepo vendorProfileRepo)
+        {
+            _vendorProfileRepo = vendorProfileRepo;
+        }
+
+        public List<VendorProfile> GetAllVendorProfiles() 
+        { 
+            return _vendorProfileRepo.GetAll();
+        }
     }
 }
