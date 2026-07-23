90e6c43cb9707cf61d8820702afd3cde4acbc918
Mohammed98112 <seaomn27@gmail.com>
2026-07-23 08:41:29 +0400

    finished QuoteRequestInviteInputDTOs

--- BuildHub/BuildHub/DTOs/QuoteRequestInviteDTOs.cs
+++ BuildHub/BuildHub/DTOs/QuoteRequestInviteDTOs.cs
@@ -2,6 +2,13 @@
 namespace BuildHub.DTOs
 {
     public class QuoteRequestInviteInputDTOs
+    {
+        [Required(ErrorMessage = "Value should not be null.")]
+        public int QuoteRequestId { get; set; }
+
+        [Required(ErrorMessage = "Value should not be null.")]
+        public int VendorProfileId { get; set; }
+    }
     
 }
 
