6ca12a1f0be8a7a7f2349a309bf84653356c8255
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-23 08:20:38 +0400

    Added the data annotation for the product model

--- BuildHub/BuildHub/Models/Product.cs
+++ BuildHub/BuildHub/Models/Product.cs
@@ -1,14 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BuildHub.Models;
 
 public class Product
 {
     [Key]
     public int productId { get; set; }
+    [Required]
     public int vendorProfileId { get; set; }
+    [Required]
     public int categoryId { get; set; }
+    [Required]
     public string name { get; set; }
+    [Required]
     public string unit { get; set; }
+    [Required]
     public int currentPrice { get; set; }
+    [Required]
     public bool isAvailable { get; set; }
 
 }
