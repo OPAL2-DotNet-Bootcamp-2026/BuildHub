2709ff206dff347a96644b5e1d5b5c0634ac70ec
Mohammed98112 <seaomn27@gmail.com>
2026-07-23 12:43:45 +0400

    finished from all attributes + constrains

--- BuildHub/BuildHub/Models/Quote.cs
+++ BuildHub/BuildHub/Models/Quote.cs
@@ -1,19 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BuildHub.Models;
 
 public class Quote
 {
     [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int quoteId { get; set; }  // system generated
-    
+
     public int quoteRequestId { get; set; }   // foreign key
     [ForeignKey("QuoteRequest")]
     public QuoteRequest QuoteRequest { get; set; }
+
     public int vendorProfileId { get; set; }  // foreign key
     [ForeignKey("VendorProfile")]
     public VendorProfile VendorProfile { get; set; }
+
     public int? parentQuoteId { get; set; }   // foreign key
     [ForeignKey("ParentQuote")]
     public Quote ParentQuote { get; set; }
+
     [Required]
     [Column(TypeName = "decimal(12,2)")]
     [Range(0, double.MaxValue)]
@@ -21,6 +27,7 @@ public class Quote
 
     [Required]
     public int durationDays { get; set; }   // user input
+
     [Required]
     [MaxLength(20)]
     public string status { get; set; } = "Pending";   // default value
