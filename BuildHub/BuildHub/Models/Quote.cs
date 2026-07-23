410bd602a5e848cb7fcc754a24b3519899f00bc2
Mohammed98112 <seaomn27@gmail.com>
2026-07-23 12:33:24 +0400

    created quoteId, primary key, constraint: automatically generated

--- BuildHub/BuildHub/Models/Quote.cs
+++ BuildHub/BuildHub/Models/Quote.cs
@@ -2,5 +2,7 @@ namespace BuildHub.Models;
 
 public class Quote
 {
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int quoteId { get; set; }  // system generated
     
 }
