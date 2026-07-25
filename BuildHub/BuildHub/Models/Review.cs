7cca9edf63fb9579134076702463bde095074748
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2026-07-25 12:12:48 +0400

    Added the attributes for review model

--- BuildHub/BuildHub/Models/Review.cs
+++ BuildHub/BuildHub/Models/Review.cs
@@ -2,5 +2,8 @@ namespace BuildHub.Models;
 
 public class Review
 {
-    
+    public int ReviewId { get; set; }
+    public int Rating { get; set; }
+    public string Comment  { get; set; }
+    public DateTime ReviewDate { get; set; }
 }
