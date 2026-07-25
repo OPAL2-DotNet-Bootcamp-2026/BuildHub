e529d80225a1510a7178463123dc60b18c542bcf
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2026-07-25 12:16:47 +0400

    Added forign key with source tags for review model

--- BuildHub/BuildHub/Models/Review.cs
+++ BuildHub/BuildHub/Models/Review.cs
@@ -3,6 +3,10 @@ namespace BuildHub.Models;
 public class Review
 {
     public int ReviewId { get; set; } //system generated
+    public int ReviewerId { get; set; } //user input 
+    public int ProductId { get; set; } //user input 
+    public int VendorProfileId { get; set; } //user input 
+    public int ContractId { get; set; } //user input 
     public int Rating { get; set; } //from list
     public string Comment  { get; set; } //user input 
     public DateTime ReviewDate { get; set; } = DateTime.UtcNow; //default value
