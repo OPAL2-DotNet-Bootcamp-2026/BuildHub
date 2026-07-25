37dc151ef4a071d7bdb0f82f86998bf7cdec9367
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2026-07-25 13:32:07 +0400

    Added services registrations (connection stering, user repo, user service ) in service container for program cs

--- BuildHub/BuildHub/Program.cs
+++ BuildHub/BuildHub/Program.cs
@@ -1,4 +1,8 @@
 
+using BuildHub.Repos;
+using BuildHub.Services;
+using Microsoft.EntityFrameworkCore;
+
 namespace BuildHub
 {
     public class Program
@@ -11,7 +15,17 @@ namespace BuildHub
             // 1. SERVICE CONTAINER 
             
             // Add services to the container.
+                        
+            builder.Services.AddDbContext<ProjectContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            
+            //repos 
+            builder.Services.AddScoped<UserRepo>();
 
+            //services 
+            builder.Services.AddScoped<UserService>();
+
+            //controllers
             builder.Services.AddControllers();
 
             var app = builder.Build();
