afcbbd9b37f575ab47708f7fb4d41558e0069991
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2026-07-25 23:20:17 +0400

    fixed frorign key annoations for Quote.cs

--- BuildHub/BuildHub/Models/Quote.cs
+++ BuildHub/BuildHub/Models/Quote.cs
@@ -8,16 +8,16 @@ public class Quote
     [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int quoteId { get; set; }  // system generated
 
-    public int quoteRequestId { get; set; }   // foreign key
     [ForeignKey("QuoteRequest")]
+    public int quoteRequestId { get; set; }   // foreign key
     public QuoteRequest QuoteRequest { get; set; }
 
-    public int vendorProfileId { get; set; }  // foreign key
     [ForeignKey("VendorProfile")]
+    public int vendorProfileId { get; set; }  // foreign key
     public VendorProfile VendorProfile { get; set; }
 
-    public int? parentQuoteId { get; set; }   // foreign key
     [ForeignKey("ParentQuote")]
+    public int? parentQuoteId { get; set; }   // foreign key
     public Quote ParentQuote { get; set; }
 
     [Required]
