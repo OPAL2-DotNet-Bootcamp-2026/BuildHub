2dc9aa2408b8410bbe0829a41250511875ab5c5c
Mohammed98112 <seaomn27@gmail.com>
2026-07-27 16:12:09 +0400

    EscrowTransactionRepo repo = new EscrowTransactionRepo apply dependency inversion

--- BuildHub/BuildHub/Services/EscrowTransactionServices.cs
+++ BuildHub/BuildHub/Services/EscrowTransactionServices.cs
@@ -5,6 +5,14 @@ namespace BuildHub.Services
 {
     public class EscrowTransactionService
     {
+        //EscrowTransactionRepo repo = new EscrowTransactionRepo();
+        //apply dependency inversion 
+        private EscrowTransactionRepo repo;
+
+        public EscrowTransactionService(EscrowTransactionRepo _repo)
+        {
+            repo = _repo;
+        }
         
     }
 }
