eae916c886d3a47f0748904b5842dce4d307d75b
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-07-27 17:44:18 +0400

    Added mock email service and used

---
+++ BuildHub/BuildHub/Services/EmailService.cs
@@ -0,0 +1,12 @@
+﻿namespace BuildHub.Services
+{
+    public class EmailService
+    {
+        public EmailService() { }
+
+        public void SendQuoteRequestAlert(int vendorId, int quoteRequestId)
+        {
+            Console.WriteLine("Email sent");
+        }
+    }
+}
