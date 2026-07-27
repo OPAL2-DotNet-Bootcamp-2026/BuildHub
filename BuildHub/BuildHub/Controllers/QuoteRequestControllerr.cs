f88312b3ba73652314edda568b7506c9cd7df822
Mohammed98112 <seaomn27@gmail.com>
2026-07-27 15:59:55 +0400

    200 - status changed

--- BuildHub/BuildHub/Controllers/QuoteRequestControllerr.cs
+++ BuildHub/BuildHub/Controllers/QuoteRequestControllerr.cs
@@ -61,7 +61,7 @@ namespace BuildHub.Controllers
             bool updated = quoteRequestService.UpdateCounte(quoteRequestId, newStatus);
             if (!updated)
                 return NotFound(); // 404 - no quote request with this id
-            return Ok("Updated successfully");
+            return Ok("Updated successfully"); // 200 - status changed
         }
 
 
