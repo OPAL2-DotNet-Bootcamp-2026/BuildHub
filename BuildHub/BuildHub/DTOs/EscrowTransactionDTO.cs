66f10dc60cc3a27d6a37a4e3a7ea0993bf3c265f
Mohammed98112 <seaomn27@gmail.com>
2026-07-27 16:32:32 +0400

    used to return escrow transaction data to the client

--- BuildHub/BuildHub/DTOs/EscrowTransactionDTO.cs
+++ BuildHub/BuildHub/DTOs/EscrowTransactionDTO.cs
@@ -4,7 +4,14 @@ namespace BuildHub.DTOs
 {
     public class EscrowTransactionOutputDTO
     {
-        
+        public int EscrowTransactionId { get; set; }
+        public int ContractId { get; set; }
+        public int? MilestoneId { get; set; }
+        public decimal Amount { get; set; }
+        public string Status { get; set; }
+        public DateTime? HeldAt { get; set; }
+        public DateTime? ReleasedAt { get; set; }
+    }
 
     public class EscrowTransactionInputDTO
     {
