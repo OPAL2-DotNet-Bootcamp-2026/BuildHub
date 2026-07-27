7e1379085687dcc45a3f5b52b77b6a1aea2d9520
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-27 12:30:24 +0400

    Add CreateNotification method to NotificationService

--- BuildHub/BuildHub/Services/NotificationService.cs
+++ BuildHub/BuildHub/Services/NotificationService.cs
@@ -12,5 +12,18 @@ using BuildHub.Repos;
         {
             this.notificationRepo = notificationRepo;
         }
+
+
+        public void CreateNotification(int userId, string title, string type)
+        {
+            Notification notification = new Notification();
+            notification.userId = userId;
+            notification.title = title;
+            notification.type = type;
+            notification.isRead = false;
+            notification.createdAt = DateTime.Now;
+
+            notificationRepo.Add(notification);
+        }
     }
 }
