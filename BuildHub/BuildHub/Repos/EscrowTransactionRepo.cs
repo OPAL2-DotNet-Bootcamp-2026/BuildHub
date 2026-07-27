6e2550785b0b6aee429683bb9ea9d928b396070e
Mohammed98112 <seaomn27@gmail.com>
2026-07-27 16:07:20 +0400

    persists changes made to an already-tracked EscrowTransaction entity

--- BuildHub/BuildHub/Repos/EscrowTransactionRepo.cs
+++ BuildHub/BuildHub/Repos/EscrowTransactionRepo.cs
@@ -5,20 +5,27 @@ namespace BuildHub.Repos
     public class EscrowTransactionRepo
     {
         private ProjectContext context;
-        
+
         public EscrowTransactionRepo(ProjectContext _context)
         {
             context = _context;
         }
+
         public void Add(EscrowTransaction escrowTransaction)
         {
             context.EscrowTransactions.Add(escrowTransaction);
             context.SaveChanges();
         }
+
         public EscrowTransaction GetByMilestoneId(int milestoneId)
         {
             return context.EscrowTransactions
                            .FirstOrDefault(e => e.milestoneId == milestoneId);
+        }
+
+        public void Update()
+        {
+            context.SaveChanges();
         }
     }
 }
