fafc389c606c7a99221e0224dd79ae0c5470596c
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-27 12:01:17 +0400

    Updated the data annotation for the notification model

--- BuildHub/BuildHub/Models/Notification.cs
+++ BuildHub/BuildHub/Models/Notification.cs
@@ -8,18 +8,22 @@ public class Notification
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int notificationId {  get; set; }
+
     [Required]
     [ForeignKey(nameof(User))]
     public int userId { get; set; }
     public User User { get; set; }
     [Required]
     [MaxLength(150)]
-    public string title { get; set; }
+    public string title { get; set; } = string.Empty;
+
     [Required]
     [MaxLength(30)]
-    public string type { get; set; }
+    public string type { get; set; } = string.Empty;
+
     [Required]
     public bool isRead { get; set; }
+
     [Required]
     public DateTime createdAt { get; set; }
 }
