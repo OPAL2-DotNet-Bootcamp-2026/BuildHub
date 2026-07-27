df4c18c963df0d96b2fa207f1178fd787dd9d63c
Mohammed98112 <seaomn27@gmail.com>
2026-07-27 16:09:09 +0400

    returns a list (not a single record) because in general one QuoteRequest--can have multiple invites

--- BuildHub/BuildHub/Repos/QuoteRequestInviteRepo.cs
+++ BuildHub/BuildHub/Repos/QuoteRequestInviteRepo.cs
@@ -9,70 +9,40 @@ namespace BuildHub.Repos
         public QuoteRequestInviteRepo(ProjectContext _context)
         {
             context = _context;
-
         }
 
         public List<QuoteRequestInvite> GetAllquoteRequestInvites()
         {
-
             return context.QuoteRequestInvites.ToList();
-
-
         }
 
         public QuoteRequestInvite GetquoteRequestInvitesById(int id)
         {
-            return context.QuoteRequestInvites.FirstOrDefault(Q => Q.inviteId == id);
+            return context.QuoteRequestInvites.FirstOrDefault(q => q.inviteId == id);
+        }
 
+        public List<QuoteRequestInvite> GetByQuoteRequestId(int quoteRequestId)
+        {
+            return context.QuoteRequestInvites
+                           .Where(q => q.quoteRequestId == quoteRequestId)
+                           .ToList();
         }
 
         public void Add(QuoteRequestInvite quoteRequestInvite)
         {
             context.QuoteRequestInvites.Add(quoteRequestInvite);
             context.SaveChanges();
-
         }
 
-
-        public void update()
+        public void Update()
         {
             context.SaveChanges();
-
         }
 
-
-        public void delete(QuoteRequestInvite quoteRequestInvite)
+        public void Delete(QuoteRequestInvite quoteRequestInvite)
         {
             context.QuoteRequestInvites.Remove(quoteRequestInvite);
             context.SaveChanges();
-
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-         
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
