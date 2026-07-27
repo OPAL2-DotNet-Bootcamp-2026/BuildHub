2c8feccde77cf7aec6128e459514b03bfc89eff3
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-27 12:24:55 +0400

    Added GetMyNotifications endpoint to NotificationController

--- BuildHub/BuildHub/Controllers/NotificationController.cs
+++ BuildHub/BuildHub/Controllers/NotificationController.cs
@@ -6,6 +6,7 @@ namespace BuildHub.Controllers
 {
     [ApiController]
     [Route("api/notifications")]
+    // [Authorize]
     public class NotificationController : ControllerBase
     {
         private NotificationService notificationService;
@@ -13,6 +14,18 @@ namespace BuildHub.Controllers
         public NotificationController(NotificationService _notificationService) //dependency injection
         {
             notificationService = _notificationService;
+        }
+
+        // GET http://localhost:5153/api/notifications/3
+        [HttpGet("{userId}")]
+        public IActionResult GetMyNotifications([FromRoute] int userId)
+        {
+            List<NotificationOutputDTO> result = notificationService.GetByUserId(userId);
+
+            if (result.Count > 0)
+                return Ok(result);
+
+            return NoContent(); //204 no data
         }
     }
 }
