d13c588f7474005c0d0e5681d42b8a2e9e027c4e
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-27 16:21:27 +0400

    Added a QuoteController with service injection

---
+++ BuildHub/BuildHub/Controllers/QuoteController.cs
@@ -0,0 +1,21 @@
+﻿using BuildHub.DTOs;
+using BuildHub.Services;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BuildHub.Controllers
+{
+    [ApiController]
+    [Route("api/quotes")]
+    public class QuoteController : ControllerBase
+    {
+        private QuoteService quoteService;
+
+        public QuoteController(QuoteService _quoteService) //dependency injection
+        {
+            quoteService = _quoteService;
+        }
+
+
+    }
+}
