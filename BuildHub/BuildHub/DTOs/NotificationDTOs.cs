574c917a4807f1d268a2a8e75c116203ff03c9c6
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-27 12:26:32 +0400

    Added NotificationOutputDTO

--- BuildHub/BuildHub/DTOs/NotificationDTOs.cs
+++ BuildHub/BuildHub/DTOs/NotificationDTOs.cs
@@ -1,6 +1,12 @@
 ﻿namespace BuildHub.DTOs
 {
-    public class NotificationDTOs
+    public class NotificationOutputDTO
     {
+        public int notificationId { get; set; }
+        public int userId { get; set; }
+        public string title { get; set; }
+        public string type { get; set; }
+        public bool isRead { get; set; }
+        public DateTime createdAt { get; set; }
     }
 }
