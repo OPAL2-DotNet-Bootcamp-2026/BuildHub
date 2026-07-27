edb9995e232dae0343a4767f2854b74a6ac4da5c
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-07-27 17:02:22 +0400

    Added review repo to allow getting reviews associted to vendorProfile (get by vendorId)

---
+++ BuildHub/BuildHub/Repos/ReviewRepo.cs
@@ -0,0 +1,19 @@
+﻿using BuildHub.Models;
+
+namespace BuildHub.Repos
+{
+    public class ReviewRepo
+    {
+        private ProjectContext _context;
+
+        public ReviewRepo(ProjectContext context)
+        {
+            _context = context;
+        }
+
+        public List<Review> GetReviewByVendorId(int vendorId)
+        {
+            return _context.Reviews.Where(r => r.VendorProfileId == vendorId).ToList();
+        }
+    }
+}
