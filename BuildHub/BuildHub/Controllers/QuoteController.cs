742bf43d9946ba68641acac9a26e53b1897d0a3d
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-27 16:23:40 +0400

    Added a GetQuoteById endpoint in the QuoteController

--- BuildHub/BuildHub/Controllers/QuoteController.cs
+++ BuildHub/BuildHub/Controllers/QuoteController.cs
@@ -16,6 +16,32 @@ namespace BuildHub.Controllers
             quoteService = _quoteService;
         }
 
+        // POST http://localhost:5153/api/quote-requests/3/quotes   (vendor submits)
+        [HttpPost("/api/quote-requests/{quoteRequestId}/quotes")]
+        public IActionResult SubmitQuote([FromRoute] int quoteRequestId, [FromBody] QuoteInputDTO input)
+        {
+            int quoteId = quoteService.SubmitQuote(
+                quoteRequestId,
+                input.vendorProfileId,
+                input.price,
+                input.durationDays
+            );
+
+            return Ok(new { quoteId = quoteId }); //200
+        }
+
+        // GET http://localhost:5153/api/quotes/3   (customer fetches)
+        [HttpGet("{quoteId}")]
+        public IActionResult GetQuoteById([FromRoute] int quoteId)
+        {
+            QuoteOutputDTO quote = quoteService.GetById(quoteId);
+
+            if (quote == null)
+                return NotFound(); // 404 notfound
+
+            return Ok(quote); //200 succeeded
+        }
+
 
     }
 }
