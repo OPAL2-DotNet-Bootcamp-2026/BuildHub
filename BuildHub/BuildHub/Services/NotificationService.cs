de070cbd77fc84bae0eb2ddc98916eeb5be4dadb
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-27 12:29:02 +0400

    Added a NotificationService class with repo injection

--- BuildHub/BuildHub/Services/NotificationService.cs
+++ BuildHub/BuildHub/Services/NotificationService.cs
@@ -1,6 +1,16 @@
+﻿using BuildHub.DTOs;
+using BuildHub.Models;
+using BuildHub.Repos;
+
 ﻿namespace BuildHub.Services
 {
     public class NotificationService
     {
+        private NotificationRepo notificationRepo;
+
+        public NotificationService(NotificationRepo notificationRepo)
+        {
+            this.notificationRepo = notificationRepo;
+        }
     }
 }
