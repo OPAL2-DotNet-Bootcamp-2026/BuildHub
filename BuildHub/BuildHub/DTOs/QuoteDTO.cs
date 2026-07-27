d64f75964beffd4ff58bcc1d2c5cfa7f83fe793d
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-27 16:07:31 +0400

    Added QuoteInputDTO in QuoteDTO.cs

---
+++ BuildHub/BuildHub/DTOs/QuoteDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuildHub.DTOs
+{
+    public class QuoteInputDTO
+    {
+        [Required]
+        public int vendorProfileId { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal price { get; set; }
+
+        [Required]
+        public int durationDays { get; set; }
+    }
+
+    public class QuoteOutputDTO
+    {
+        public int quoteId { get; set; }
+        public int quoteRequestId { get; set; }
+        public int vendorProfileId { get; set; }
+        public decimal price { get; set; }
+        public int durationDays { get; set; }
+        public string status { get; set; }
+        public DateTime submittedAt { get; set; }
+    }
+}
