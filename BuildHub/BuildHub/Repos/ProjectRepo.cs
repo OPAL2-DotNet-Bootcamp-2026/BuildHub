b577b04892e8bc74f16cae3e25f7ac6b8341d451
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-07-28 18:31:31 +0400

    Implemented project repo with adding method

---
+++ BuildHub/BuildHub/Repos/ProjectRepo.cs
@@ -0,0 +1,20 @@
+﻿using BuildHub.Models;
+
+namespace BuildHub.Repos
+{
+    public class ProjectRepo
+    {
+        private ProjectContext _context;
+
+        public ProjectRepo(ProjectContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Project project)
+        {
+            _context.Projects.Add(project);
+            _context.SaveChanges();
+        }
+    }
+}
