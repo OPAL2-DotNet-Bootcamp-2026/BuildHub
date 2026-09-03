b2f56e768004091547feb58e712c92579b6599ee
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2026-09-04 00:39:59 +0400

    Added connection string and implemented the db context in program container

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,4 +1,7 @@
 
+using Backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Backend
 {
     public class Program
@@ -8,6 +11,10 @@ namespace Backend
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            builder.Services.AddDbContext<BuildHubDbContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
