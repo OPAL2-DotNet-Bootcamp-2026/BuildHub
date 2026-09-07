eac586874bca78ef6847ca96921ab6bf225462dc
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-09-07 16:25:56 +0400

    updated variable name

--- FrontEnd/components/Accept_Offer.js
+++ FrontEnd/components/Accept_Offer.js
@@ -41,7 +41,7 @@ if (!response.ok) {
     }
 
 // Convert backend JSON into a JavaScript object
-    const quote = await response.json();
+    const offer  = await response.json();
 
  // Show the result in the Console , because i want to check the get work or not
     console.log("GET successful:", offer);
@@ -69,7 +69,7 @@ if (!token) {
     alert("Please log in first.");
     window.location.href = "login.html";
     return;
-    }
+  }
 
 
 acceptButton.disabled = true;
